71171123f1612fafd1cd31f81e44b26d84dc92c5
Pulkit219 <ascension1110@gmail.com>
2018-01-17 23:40:36 -0500

    47th commit
    Vesco added CSS/HTML to Team section

--- Vesco website project/js/custom.js
+++ Vesco website project/js/custom.js
@@ -1,7 +1,40 @@
 /*=========================
 SERVICES
 =======================*/
-$(function(){
-    
-   new WOW().init();
+$(function () {
+
+    new WOW().init();
+
+});
+
+
+/*=========================
+WORK
+=======================*/
+
+$(function () {
+    $('#work').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+
+    });
+
+});
+
+/*=========================
+TEAM
+=======================*/
+$(function () {
+
+    $(".owl-carousel").owlCarousel({
+        items:3,
+        autoplay:true,
+        loop:true
+    }
+      
+    );
+
 });
